191659afbd5a364e7e2ead3d5bc462b8cd34fe74
staljabro <jsb9525@hotmail.com>
2026-09-25 18:52:25 +1000

    v0.1.4

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 // Update this value for each user-facing release.
-export const APP_VERSION = '0.1.3 (Alpha)'
+export const APP_VERSION = '0.1.4 (Alpha)'
 export const GITHUB_REPOSITORY_URL = 'https://github.com/staljabro/stage-layout-app'
